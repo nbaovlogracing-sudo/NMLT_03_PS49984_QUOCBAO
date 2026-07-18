88b9194b812c471bdf5bc17c45a4c8bdb36b4c21
nbaovlogracing-sudo <nbaovlogracing@gmail.com>
2026-07-18 10:19:48 +0700

    Hoan Thanh Lab2 Bai2

--- Lab1/Lab2/Bai2_TichDiem.c
+++ Lab1/Lab2/Bai2_TichDiem.c
@@ -6,17 +6,17 @@ int main(){
     // Nhập điểm số
     printf("TINH DIEM TRUNG BINH (Toan he so 3, Ly he so 2, Hoa he so 1)");
     printf("Nhap diem toan: ");
-    scanf("%d", &toan);
+    scanf("%f", &toan);
     printf("Nhap diem ly: ");
-    scanf("%d", &ly);
+    scanf("%f", &ly);
     printf("Nhap diem hoa: ");
-    scanf("%d", &hoa);
+    scanf("%f", &hoa);
 
     // Tính điểm trung bình
     dtb = (toan * 3 + ly * 2 + hoa) / 6;
 
     // Hiển thị kết quả
-    printf("\nDiem trung binh: %2.f\n",dtb);
-    
+    printf("\nDiem trung binh: %.2f\n",dtb);
+
     return 0;
 }
